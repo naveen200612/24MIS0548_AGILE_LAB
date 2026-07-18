78b4905d3198fee7f0754293d61328f5bf02fe71
naveen200612 <naveenkumar.e2024@vitstudent.ac.in>
2026-07-18 17:40:58 +0530

    Case Study 2 completed

--- case2_registration.py
+++ case2_registration.py
@@ -4,5 +4,3 @@ name = input("Enter Patient Name: ")
 age = input("Enter Patient Age: ")
 
 print("Patient Registered Successfully")
-phone = input("Enter Phone Number: ")
-print("Phone Number:", phone)
